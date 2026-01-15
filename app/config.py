f7d7083323d0eac9f189cf7864f31cd0ca432030
initaman <amandabral2104@gmail.com>
2026-01-15 11:37:00 +0530

    Added BaseSettings in app/config.py to read env vars

---
+++ app/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings 
+
+class Settings(BaseSettings):
+    app_name: str = "FastAPI Jenkins Project"
+    version: str = "1.0.0"
+    debug: bool = False 
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
